2e79c636f8601d8834760f2b5bf29d517a2281fb
Anshul bhathija <anshulbhathija966@gmail.com>
2026-04-02 00:19:46 +0530

    feat: "config file change"

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 // Production: export const API_BASE_URL = 'https://hackaccino-backend-2x2zepq5nq-el.a.run.app';
-export const API_BASE_URL = 'http://localhost:3001';
+export const API_BASE_URL = 'https://hackaccino-backend-2x2zepq5nq-el.a.run.app';
